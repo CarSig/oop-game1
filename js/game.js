c3f31227376d425cab419ada0034745ad1659de4
Lovro Boric <lovro.boric@gmail.com>
2023-02-10 18:43:09 +0100

    formating

--- js/game.js
+++ js/game.js
@@ -1,29 +1,17 @@
-
-
-
-
-
-
-
-
 class Board {
     constructor() {
         this.width = 800;
         this.height = 800;
         this.domElement = null;
         this.createDomElement();
-        console.log("here")
+        console.log("here");
     }
 
     createDomElement() {
-        this.domElement = document.createElement('div');
-
+        this.domElement = document.createElement("div");
         this.domElement.style.width = this.width + "px";
         this.domElement.style.height = this.height + "px";
         this.domElement.style.border = "13px solid red";
-        // const bodyElm = document.querySelector('body');
-        // bodyElm.appendChild(this.domElement);
+
     }
 }
-
-
